1b3aabef027c2d1f7616ffaf5897a75ed63c654d
praveenkalla <praveen.kalla@icentris.com>
2022-05-17 17:48:00 +0530

    Final Commit test cases

--- src/main/java/com/capgemini/recipes/controller/RecipeController.java
+++ src/main/java/com/capgemini/recipes/controller/RecipeController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -80,6 +81,7 @@ public class RecipeController {
 	 * 
 	 * @param id - Recipe ID
 	 */
+	@DeleteMapping("/{id}")
 	public void delete(@PathVariable Long id) {
 		log.debug(":::::::In delete method :::::::::::");
 		recipeService.delete(id);
--- src/test/java/com/capgemini/recipes/service/RecipeServiceTest.java
+++ src/test/java/com/capgemini/recipes/service/RecipeServiceTest.java
@@ -1,7 +1,10 @@
 package com.capgemini.recipes.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -9,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,6 +26,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.capgemini.recipes.dto.RecipeDto;
+import com.capgemini.recipes.exceptions.ResourceNotFoundException;
 import com.capgemini.recipes.model.Ingredient;
 import com.capgemini.recipes.model.Recipe;
 import com.capgemini.recipes.repository.RecipeRepository;
@@ -77,4 +82,30 @@ class RecipeServiceTest {
 		RecipeDto recipes = recipeService.findById(anyLong());
 		Mockito.verify(recipeRepository, Mockito.times(1)).findById(anyLong());
 	}
+	
+	@Test
+	public void testRecipeByIdNotFound() {
+		ResourceNotFoundException thrown = assertThrows(
+				ResourceNotFoundException.class,
+		           () -> recipeService.findById(123L),
+		           "Expected findbyId() to throw");
+
+	    assertTrue(thrown.getMessage().contains("Recipe with ID"));
+	}
+	
+	@Test
+	void deleteById() {
+	    Recipe recipe = new Recipe();
+	    when(recipeRepository.findById(1L)).thenReturn(Optional.of(recipe));
+	    recipeService.delete(1L);
+	    verify(recipeRepository).findById(1L);
+	}
+	
+	@Test
+	void deleteActorByIdThrowsExceptionIfIDNotFound() {
+		Assertions.assertThatExceptionOfType(ResourceNotFoundException.class)
+	        .isThrownBy(() -> recipeService.delete(1L))
+	        .withMessage("Recipe with ID: 1 Not Found!");
+	}
+
 }
