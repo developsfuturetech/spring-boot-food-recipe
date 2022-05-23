1938260a3727c31ac84afad61f1b8d1a5cb9f77b
Praveen Kumar K <developsfuturetech@gmail.com>
2022-05-23 15:43:09 +0530

    Update SwaggerConfig.java

--- src/main/java/com/capgemini/recipes/config/SwaggerConfig.java
+++ src/main/java/com/capgemini/recipes/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.regex("/capgemini/api/.*"))
+                .paths(PathSelectors.regex("/cap/api/.*"))
                 .build()
                 .apiInfo(apiInfo());
                 
